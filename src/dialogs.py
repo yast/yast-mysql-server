527601900e36127ed1fb6430ffea41307ec8a870
Michal Žugec <mzugec@suse.cz>
2008-06-15 15:27:19 +0000

    store all selected items into "selected_items" ;-)
    svn path=/trunk/mysql-server/; revision=48316

--- src/dialogs.py
+++ src/dialogs.py
@@ -34,41 +34,60 @@ from defaults import SecuritySettings
 
 caption = gettext.gettext('MySQL Server Configuration')
 
-
+selected_items={}
 # ShowSequenceDialog
 # 	contents	=
 #	values		= Button/TextBox/... values
 #	help		= the dialog's help text
-#	back		= show 'back' button or not
+# back = enable or disable `Back button
+#	wanted		= items we are insterested about
 
-def ShowSequenceDialog(contents, values, help, back):
+def ShowSequenceDialog(contents, values, help, back,  wanted):
         
 #        Wizard.SetContentsButtons(caption, contents, help, Label.BackButton(), Label.NextButton())
-    	Wizard.SetContentsButtons(caption, contents, help, "back", "next")
+    Wizard.SetContentsButtons(caption, contents, help, "back", "next")
 #    	Wizard.SetTitleIcon('yast-mysql')
 
+	# default values
+	# need to be read from my.cnf
+    if selected_items.__contains__(wanted[0]):
+        values={}
+        for item in wanted:
+            type=UI.QueryWidget(Term('id',  item), Symbol('WidgetClass') )
+            if ('YInputField',  'YCheckBox',  'YSelectionBox').__contains__(type):
+                values[item]=selected_items[item]
+            else :
+                values[selected_items[item]]=True
+#            values = {selected_items['ServerType']:True}
 
-	for ID in values.keys():
-		UI.ChangeWidget(Term('id',  ID), Symbol('Value'), values[ID])
 
-	if not back:
-    		Wizard.DisableBackButton()
+
+    for ID in values.keys():
+        UI.ChangeWidget(Term('id',  ID), Symbol('Value'), values[ID])
+
+    if not back:
+        Wizard.DisableBackButton()
 		
-    	ret = None;
-    	while True :
-		ret = UI.UserInput()
+    ret = None;
+    while True :
+            ret = UI.UserInput()
 
-		#abort
-		if ret.value == 'abort':
-	    		break
-
-		#next
-		elif ret.value == 'next':
-		  	break
-       
+            #back
+            if ret.value == 'back':
+                for item in wanted:
+                    if selected_items.__contains__(item):
+                        selected_items.__delitem__(item)
+                break
+            #next
+            elif ret.value == 'next':
+                for item in wanted:
+                    selected_items[item]=UI.QueryWidget(Term('id', item), Symbol('Value'))
+                break
+            #abort    
+            elif ret.value=="abort":
                 break
 
-	return ret;
+    return ret;
 
 
 def ServerTypeDialog():
@@ -95,20 +114,12 @@ def ServerTypeDialog():
 	     )
     )
 
-	# default values
-	# need to be read from my.cnf
-	values = {'Server':True}
-
-	# help text for this dialog
-	help = 'Server Configuration...'
-
-	# bring it to the screen...
-        ret = ShowSequenceDialog(contents, values, help, False);
-
-        
-    
-	# chose the settings depending on which button the user has pressed
-	selection = ServerTypeSettings[UI.QueryWidget(Term('id', 'ServerType'), Symbol('CurrentButton'))]
+        # help text for this dialog
+        help = 'Server Configuration...'
+        defaults={'Server':True}
+        wanted=['ServerType']
+        # bring it to the screen...
+        ret = ShowSequenceDialog(contents, defaults, help, False, wanted);
 
 	return ret
 	
@@ -134,13 +145,13 @@ def DatabaseUsageDialog():
 	     )
 	)
 
-	values = {'Transactional':True}
     
-	help = 'Server Configuration...'
+        help = 'Server Configuration...'
+        defaults = {'Transactional':True}
+        wanted=['DatabaseUsage']
+    	ret = ShowSequenceDialog(contents, defaults, help, True,  wanted);
 
-    	ret = ShowSequenceDialog(contents, values, help, True);
-
-	selection = DatabaseUsageSettings[UI.QueryWidget(Term('id', 'DatabaseUsage'), Symbol('CurrentButton'))]
+#	selected_items['DatabaseUsage']=UI.QueryWidget(Term('id', 'DatabaseUsage'), Symbol('CurrentButton'))
 
 	return ret
 
@@ -169,21 +180,23 @@ def ServerConnectionsDialog():
 	     )
 	)
     
-	values = {'Decision':True}
 	
-	help = 'Server Configuration...'
+        help = 'Server Configuration...'
+        defaults = {'Decision':True}
+        wanted=['ServerConnections',   'Connections']
 
-    	ret = ShowSequenceDialog(contents, values, help, True);
+    	ret = ShowSequenceDialog(contents, defaults, help, True,  wanted);
 
-	selection = ServerConnectionSettings[UI.QueryWidget(Term('id',  'ServerConnections'), Symbol('CurrentButton'))]
+#	selected_items['ServerConnections']=UI.QueryWidget(Term('id',  'ServerConnections'), Symbol('CurrentButton'))
+#        selected_items['Connections']=UI.QueryWidget(Term('id',  'Connections'), Symbol('Value'))
 
 	return ret
 
 def ServerNetworkingDialog():
-        from ycp import *
-        ycp.widget_names()
+    from ycp import *
+    ycp.widget_names()
 
-    	contents = VBox(
+    contents = VBox(
 	     Left(Term('Label','Networking Options')),
 	     VBox(
 		VSpacing(1),
@@ -197,23 +210,20 @@ def ServerNetworkingDialog():
 		VStretch()
 	     )
 	)
-    
-	values = {'TCPIP':True, 'Strict':True}
 
-	help = 'Server Configuration...'
+    help = 'Server Configuration...'
+    defaults = {'TCPIP':True, 'Strict':True}
+    wanted=[ 'TCPIP',  'Strict' ]
 
-    	ret = ShowSequenceDialog(contents, values, help, True);
-        tcpip=UI.QueryWidget(Term('id', 'TCPIP'),  Symbol('Value'))
-        print tcpip
-        CharacterSetSettings
+    ret = ShowSequenceDialog(contents, defaults, help, True,  wanted);
 
-	return ret
+    return ret
 
 def CharacterSetDialog(): 
-        from ycp import *
-        ycp.widget_names()
+    from ycp import *
+    ycp.widget_names()
 
-    	contents = VBox(
+    contents = VBox(
 	     Left(Term('Label','Character Set')),
 	     VBox(
 		VSpacing(1),
@@ -234,21 +244,20 @@ def CharacterSetDialog():
 	     )
 	)
 
-	values = {'Standard':True}
  
-	help = 'Server Configuration...'
+    help = 'Server Configuration...'
+    defaults = {'Standard':True}
+    wanted=['CharacterSet',  'charset']
 
-    	ret = ShowSequenceDialog(contents, values, help, True);
+    ret = ShowSequenceDialog(contents, defaults, help, True,  wanted);
 
-	selection = CharacterSetSettings[UI.QueryWidget(Term('id',  'CharacterSet'), Symbol('CurrentButton'))]
-
-	return ret
+    return ret
 
 def SecurityConfigurationDialog(): 
-        from ycp import *
-        ycp.widget_names()
+    from ycp import *
+    ycp.widget_names()
 
-    	contents = VBox(
+    contents = VBox(
 	     Left(Term('Label','Security Options')),
 	     VBox(
 		VSpacing(1),
@@ -268,11 +277,10 @@ def SecurityConfigurationDialog():
 	     )
 	)
     
-	help = 'Server Configuration...'
-	
-	values = {'AnonymousAccount':True}
+    help = 'Server Configuration...'
+    defaults = {'AnonymousAccount':True}
+    wanted=['Modify',  'AnonymousAccount',  'OldPassword',  'NewPassword',  'Confirm']
+    ret = ShowSequenceDialog(contents, defaults, help, True,  wanted);
 
-    	ret = ShowSequenceDialog(contents, values, help, True);
-
-	return ret
+    return ret
 
